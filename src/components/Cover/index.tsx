695b5bb277c00a91e95304ae1c9ea9671375b36e
[jutrong] <[leek71358@gmail.com]>
2024-06-28 14:08:18 +0900

    Design: 반응형 디자인

--- src/components/Cover/index.tsx
+++ src/components/Cover/index.tsx
@@ -10,34 +10,38 @@ const Cover = ({ onMoveBox, onAccordianMoveBox, onContactMoveBox }: { onMoveBox:
       <div className="absolute top-0 left-0 w-full h-full z-0">
         <Image src="/images/png/pc-background.png" fill={true} alt="washfun cover" quality={100} style={{ objectFit: "cover" }} />
       </div>
-      <div className="absolute inset-x-0 w-full h-24 z-10 flex justify-between items-center px-60">
-        <div>
-          <Image src="/images/svg/white-logo.svg" alt="washfun Logo" width={46} height={46} />
+      <div className="absolute top-0 left-0 w-full h-full z-0 hidden lg:block">
+        <Image src="/images/png/mobile-background.png" fill={true} alt="washfun cover" quality={100} style={{ objectFit: "cover" }} />
+      </div>
+      <div className="absolute inset-x-0 w-full h-24 z-10 flex justify-between items-center px-60 lg:px-10">
+        <div className="w-[120px] h-[60px] flex justify-center items-center">
+          <Image src="/images/svg/white-logo.svg" alt="washfun Logo" width={60} height={50} />
         </div>
-        <div className="w-96 h-px bg-gray z-10 mx-48"></div>
-        <div className="flex space-x-12 cursor-pointer">
+        <div className="flex space-x-12 cursor-pointer w-full justify-end lg:hidden">
           <p className="text-white text-opacity-80 hover:text-opacity-100 transition duration-500" onClick={onMoveBox}>주요 기능</p>
           <p className="text-white text-opacity-80 hover:text-opacity-100 transition duration-500" onClick={onAccordianMoveBox}>자주 묻는 질문</p>
         </div>
       </div>
-      <div className="absolute top-72 left-72 flex flex-col text-white">
-        <div className="flex flex-col gap-3 mb-16">
-          <p className="text-7xl font-medium  opacity-90">셀프 세차의</p>
-          <p className="text-8xl font-bold tracking-wide">디지털 혁신</p>
+      <div className="absolute top-72 left-72 flex flex-col text-white lg:left-1/2 lg:-translate-x-1/2 lg:top-40 w-full">
+        <div className="flex flex-col gap-3 mb-16 lg:items-center">
+          <p className="text-7xl font-medium  opacity-90 lg:text-6xl">셀프 세차의</p>
+          <p className="text-8xl font-bold tracking-wide lg:text-6xl">디지털 혁신</p>
         </div>
-        <div className="mb-6 flex">
-          <p className="text-4xl text-primary">워시펀</p>
-          <span className="text-4xl text-white">과 함께</span>
+        <div className="mb-6 flex  lg:justify-center">
+          <p className="text-4xl text-primary lg:text-3xl">워시펀</p>
+          <span className="text-4xl text-white lg:text-3xl">과 함께</span>
         </div>
-        <motion.div
-          whileHover={{ scale: 1.05, transition: { duration: 0.5 } }}
-          className="flex items-center gap-3 p-3 w-40 h-12 border border-x-2 bg-white rounded-xl z-20" onClick={onContactMoveBox}>
+        <div className=" flex lg:justify-center">
           <motion.div
-          >
-            <Image src="/images/svg/contact.svg" width={30} height={30} alt="contact" />
+            whileHover={{ scale: 1.05, transition: { duration: 0.5 } }}
+            className="flex items-center gap-3 p-3 w-40 h-12 border border-x-2 bg-white rounded-xl z-20 lg:justify-center " onClick={onContactMoveBox}>
+            <motion.div
+            >
+              <Image src="/images/svg/contact.svg" width={30} height={30} alt="contact" />
+            </motion.div>
+            <p className="text-lg text-gray cursor-pointer lg:text-base">문의하기</p>
           </motion.div>
-          <p className="text-1xl text-gray cursor-pointer">문의하기</p>
-        </motion.div>
+        </div>
       </div>
       <motion.div
         className="absolute bottom-10 left-1/2 w-10 h-10 z-10 "
--- src/components/VarienceFeeService/index.tsx
+++ src/components/VarienceFeeService/index.tsx
@@ -31,18 +31,18 @@ const VarienceFeeService = () => {
   return (
     <>
       <motion.div ref={scrollRef} className="h-250vh w-full relative" style={{ backgroundColor }} >
-        <div className="sticky top-0 z-20 h-screen overflow-hidden">
+        <div className="sticky top-0 left-0 z-20 h-screen overflow-hidden">
           <motion.div
-            className="flex justify-center" >
-            <motion.div className="text-black mt-40 z-20" style={{ color: textColor }}>
-              <p className="font-bold text-5xl">시간별 변동 요금제로 매장 회전율 극대화</p>
-              <motion.div className="flex flex-col items-center mt-10 font-light opacity-70" style={{ color: subTextColor }}>
+            className="flex justify-center  md:w-full" >
+            <motion.div className="text-black mt-40 z-20 md:mt-30 md:px-4" style={{ color: textColor }}>
+              <p className="font-bold text-5xl md:text-xl md:flex md:justify-center ">시간별 변동 요금제로 매장 회전율 극대화</p>
+              <motion.div className="flex flex-col items-center mt-10 font-light opacity-70 md:text-xs" style={{ color: subTextColor }}>
                 <p>바쁜 시간에는 기존 요금을</p>
                 <p>한가한 시간에는 요금 할인으로 오래쓰시는 고객분들을 한가한 시간으로 유도하여 회전율을 높이고,</p>
                 <p>주변 세차장과의 가격 경쟁력 확보로 고객 추가 유입 !</p>
               </motion.div>
             </motion.div>
-            <div className="absolute bottom-0 left-1/2 -translate-x-1/2 ">
+            <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
               <motion.div className="absolute" style={{ opacity: dayPannelOpacity }}>
                 <Image src="/images/svg/pannel-day100.svg" width={650} height={100} alt="pannel-day" />
               </motion.div>
@@ -78,7 +78,7 @@ const VarienceFeeService = () => {
                 </motion.div>
               </div>
               <motion.div className="absolute top-2/4 left-1/2 -translate-x-1/2" style={{ opacity: dayPannelOpacity }}>
-                <div className="w-600px h-32 bg-lightPrimary shadow-primary-shadow rounded-3xl flex justify-between items-center px-10">
+                <div className="w-600px h-32 bg-lightPrimary shadow-primary-shadow rounded-3xl flex justify-between items-center px-10 md:w-[300px] md:h-20">
                   <p className="text-gray font-Jamsil text-4xl opacity-80">Day</p>
                   <div className="flex justify-center items-center">
                     <motion.div className="text-black font-Jamsil text-4xl" style={{ opacity: dayPercentOpacity }}>100%</motion.div>
@@ -86,7 +86,7 @@ const VarienceFeeService = () => {
                 </div>
               </motion.div>
               <motion.div className="absolute top-2/4 left-1/2 -translate-x-1/2" style={{ opacity: nightPercentOpacity }}>
-                <div className="w-600px h-32 bg-lightPrimary shadow-primary-shadow rounded-3xl flex justify-between items-center px-10">
+                <div className="w-600px h-32 bg-lightPrimary shadow-primary-shadow rounded-3xl flex justify-between items-center px-10 md:w-[300px] md:h-20">
                   <p className="text-gray font-Jamsil text-4xl opacity-80">Night</p>
                   <div className="flex justify-center items-center">
                     <motion.div className="relative w-full h-full text-lightPrimary  font-Jamsil text-4xl opacity-5"
@@ -100,10 +100,10 @@ const VarienceFeeService = () => {
                 </div>
               </motion.div>
             </div>
-            <motion.div className="absolute left-80 top-1/3" style={{ x: cloudX }}>
+            <motion.div className="absolute left-80 top-1/3 opacity-50 md:top-[60px]" style={{ x: cloudX }}>
               <Image src="/images/svg/cloud.svg" width={140} height={120} alt="cloud" />
             </motion.div>
-            <motion.div className="absolute right-28 top-40" style={{ x: sunX }}>
+            <motion.div className="absolute right-28 top-40 opacity-50 md:top-[320px] md:right-[300px]" style={{ x: sunX }}>
               <Image src="/images/svg/sun.svg" width={160} height={120} alt="sun" />
             </motion.div>
             <motion.div className="absolute bottom-0 left-0 -z-10" style={{ opacity: buildingDayOpacity }}>
@@ -115,10 +115,10 @@ const VarienceFeeService = () => {
             <motion.div className="absolute bottom-10 left-0 -z-10" style={{ opacity: buildingNightOpacity }}>
               <Image src="/images/svg/building-night.svg" width={2000} height={100} alt="building" />
             </motion.div>
-            <motion.div className="absolute right-28 top-40" style={{ x: moonX }}>
+            <motion.div className="absolute right-28 top-40 opacity-50 md:top-[50px]" style={{ x: moonX }}>
               <Image src="/images/png/moon.png" alt="moon" width={200} height={120} />
             </motion.div>
-            <motion.div className="absolute left-28 top-28" style={{ x: moonXX }}>
+            <motion.div className="absolute left-28 top-28 opacity-50 md:top-[320px]" style={{ x: moonXX }}>
               <Image src="/images/png/moon.png" alt="moon" width={140} height={120} />
             </motion.div>
             <motion.div className="absolute bottom-16 -z-20 opacity-10" style={{ y: upY }}>
