530e732665e4c91c3fd6425a1714777cf80ec08c
[jutrong] <[leek71358@gmail.com]>
2024-07-01 16:10:32 +0900

    Feat: 개인정보 약관 추가

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+const PrivacyPolicy = () => {
+  return (
+    <div className="h-full w-full flex justify-center">
+      <div className="flex w-[1200px] justify-center">
+        <Image src="/images/png/desktop-policy.png" width={1900} height={5000} alt="policy" />
+      </div>
+    </div>
+  )
+}
+
+export default PrivacyPolicy;
