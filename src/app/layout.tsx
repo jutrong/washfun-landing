5e32908afbc1d6fca0862edd662d6dda062e563f
jutrong <leek71358@gmail.com>
2024-07-02 08:17:45 +0000

    Chore: metadata opengraph 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,10 +12,23 @@ const pretendard = localFont({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://washfun.vercel.app"),
+
   title: "WashFun - 워시펀",
   description: "셀프 세차의 디지털 혁신 - 워시펀과 함께",
   icons: {
     icon: "/images/svg/washfun-logo2.svg"
+  },
+  openGraph: {
+    title: "WashFun - 워시펀",
+    description: "셀프 세차의 디지털 혁신 - 워시펀과 함께",
+    images: [
+      {
+        url: "/images/png/dashboard-mockup.png",
+        alt: "워시펀 대시보드 이미지",
+      }
+    ]
+
   }
 };
 
@@ -33,8 +46,12 @@ export default function RootLayout({
       <meta property="og:url" content="https://washfun.vercel.app/" />
       <meta property="og:type" content="website" />
       <meta property="og:image" content="/images/png/dashboard-mockup.png" />
-      <meta property="og:title" content="워시펀" />
       <meta property="og:description" content="워시펀을 통해 매출,고객 수 등 다양한 데이터를 한 눈에 확인하세요." />
+      <meta property="og:site_name" content="washfun - 워시펀" />
+      <meta property="og:locale" content="ko_KR" />
+
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
       <body className={pretendard.className}>
         {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
           <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
