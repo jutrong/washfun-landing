603df38213e45f6effb6347204c9336193ea842f
jutrong <leek71358@gmail.com>
2024-07-23 03:06:33 +0000

    Chore: og이미지 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,10 +42,10 @@ export default function RootLayout({
       <meta name="google-site-verification" content="rFOt_n-IDumlpzicrWEfpl9ctFdZW62a2IkNIanTBaM" />
       <meta name="naver-site-verification" content="d983fee3f0b7db68efd9a65aa2ebd48490c11d92" />
       {/* <!-- naver 블로그, 카카오톡 미리보기 설정 --> */}
-      <meta property="og:title" content="워시펀 - 셀프 세차의 디지털 혁신 - 워시펀과 함께" />
+      <meta property="og:title" content="셀프 세차의 디지털 혁신 - 워시펀과 함께" />
       <meta property="og:url" content="https://www.lifedivergence.com" />
       <meta property="og:type" content="website" />
-      <meta property="og:image" content="/images/png/dashboard-mockup.png" />
+      <meta property="og:image" content="/images/png/cover-page.png" />
       <meta property="og:description" content="워시펀을 통해 매출,고객 수 등 다양한 데이터를 한 눈에 확인하세요." />
       <meta property="og:site_name" content="washfun - 워시펀" />
       <meta property="og:locale" content="ko_KR" />
