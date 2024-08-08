8e1db2b8e0be05d7bf1479e7fdfbe7523cc235e8
jutrong <leek71358@gmail.com>
2024-08-08 08:27:46 +0000

    Feat: Webview 테스트

---
+++ src/app/inquiry/page.tsx
@@ -0,0 +1,10 @@
+const Inquiry = () => {
+  return (
+    <div className="w-screen justify-center text-black p-20">
+      <h1 className="font-bold text-xl">만약 이메일을 등록하지 않았다면 아래 내용과 함께 고객센터에 문의 남겨 주세요.</h1>
+      <p className="mt-2">아래 내용이 확인되지 않으면 워시펀 팀에서 번호를 변경할 수 없어요.</p>
+    </div>
+  )
+}
+
+export default Inquiry;
