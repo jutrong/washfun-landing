7fef1fd80bdadcf54b5efee6d6839ae7a9ac3da9
[jutrong] <[leek71358@gmail.com]>
2024-08-12 18:55:04 +0900

    Fix: 이미지 수정

--- src/app/(webview)/term-of-service/page.tsx
+++ src/app/(webview)/term-of-service/page.tsx
@@ -4,7 +4,7 @@ const TermsOfService = () => {
   return (
     <div className="size-full flex justify-center">
       <div className="flex w-[1200px] justify-center">
-        <Image src="/images/png/termsOfService.png" width={1900} height={5000} alt="policy" />
+        <Image src="/images/png/termsOfService.png" width={1900} height={10000} alt="policy" />
       </div>
     </div>
   )
