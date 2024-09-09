1c5fc290bdc960a62df7f417d1570b80a34f108f
[jutrong] <[leek71358@gmail.com]>
2024-09-09 16:52:47 +0900

    Fix: png naming fix

--- src/app/(webview)/term-of-service/page.tsx
+++ src/app/(webview)/term-of-service/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const TermsOfService = () => {
   return (
-    <div className="size-full flex justify-center">
+    <div className="flex size-full justify-center">
       <div className="flex w-[1200px] justify-center">
         <Image src="/images/png/termsOfService.png" width={1900} height={10000} alt="policy" />
       </div>
--- src/components/Home/ServiceInfo/index.tsx
+++ src/components/Home/ServiceInfo/index.tsx
@@ -3,29 +3,29 @@ import { motion } from "framer-motion"
 
 const ServiceInfo = () => {
   return (
-    <div className="w-full h-full bg-gradient-to-t from-lightPrimary to-white pb-40 overflow-x-hidden">
+    <div className="size-full overflow-x-hidden bg-gradient-to-t from-lightPrimary to-white pb-40">
       <div className="flex justify-center pt-40 text-5xl font-bold tracking-wider md:text-3xl">
-        <p className="text-black mr-4">셀프 셀차의</p>
-        <span className="text-black text-gradient">패  러  다  임</span>
+        <p className="mr-4 text-black">셀프 셀차의</p>
+        <span className="text-gradient text-black">패  러  다  임</span>
       </div>
-      <motion.div className="flex justify-center items-center gap-20 mt-32 lg:gap-6 md:flex-col">
-        <motion.div className="w-80 h-96 shadow-primary-shadow rounded-3xl flex justify-start items-center flex-col lg:w-60 md:w-80"
+      <motion.div className="mt-32 flex items-center justify-center gap-20 lg:gap-6 md:flex-col">
+        <motion.div className="flex h-96 w-80 flex-col items-center justify-start rounded-3xl shadow-primary-shadow lg:w-60 md:w-80"
           initial={{ opacity: 0, y: 100 }}
           whileInView={{ opacity: 1, y: 0, transition: { duration: 2, delay: 0.2 } }}
         >
           <div className=" mt-10">
             <Image src="/images/png/NFC.png" width={200} height={200} alt="nfc" />
           </div>
-          <p className="text-black text-2xl font-bold mt-4">사용량 기반</p>
-          <p className="text-black text-2xl font-bold">후불 결제 시스템</p>
+          <p className="mt-4 text-2xl font-bold text-black">사용량 기반</p>
+          <p className="text-2xl font-bold text-black">후불 결제 시스템</p>
         </motion.div>
-        <motion.div className="w-80 h-96 shadow-primary-shadow rounded-3xl  flex justify-start items-center flex-col lg:w-60 md:w-80"
+        <motion.div className="flex h-96 w-80 flex-col  items-center justify-start rounded-3xl shadow-primary-shadow lg:w-60 md:w-80"
           initial={{ opacity: 0, y: 100 }}
           whileInView={{ opacity: 1, y: 0, transition: { duration: 2, delay: 0.4 } }}
         >
           <div className="relative mt-10">
             <Image src="/images/svg/coinbox.svg" width={180} height={200} alt="coinbox" />
-            <motion.div className="absolute top-0 right-5"
+            <motion.div className="absolute right-5 top-0"
               initial={{ y: 0 }}
               animate={{ y: [0, -10, 0, -10, 0] }}
               transition={{ duration: 4, repeat: Infinity }}
@@ -33,17 +33,17 @@ const ServiceInfo = () => {
               <Image src="/images/svg/wifi.svg" width={25} height={200} alt="coinbox" />
             </motion.div>
           </div>
-          <p className="text-black text-2xl font-bold mt-10">실시간 서버와 통신하는</p>
-          <p className="text-black text-2xl font-bold">IoT 코인박스 · 청소기</p>
+          <p className="mt-10 text-2xl font-bold text-black">실시간 서버와 통신하는</p>
+          <p className="text-2xl font-bold text-black">IoT 코인박스 · 청소기</p>
         </motion.div>
-        <motion.div className="w-80 h-96 shadow-primary-shadow rounded-3xl flex justify-start items-center flex-col lg:w-60 md:w-80"
+        <motion.div className="flex h-96 w-80 flex-col items-center justify-start rounded-3xl shadow-primary-shadow lg:w-60 md:w-80"
           initial={{ opacity: 0, y: 100 }}
           whileInView={{ opacity: 1, y: 0, transition: { duration: 2, delay: 0.6 } }}
         >
           <div className="mt-10">
             <Image src="/images/png/data.png" width={200} height={200} alt="data" />
           </div>
-          <p className="text-black text-2xl font-bold mt-4">사용량 데이터 수집</p>
+          <p className="mt-4 text-2xl font-bold text-black">사용량 데이터 수집</p>
         </motion.div>
       </motion.div>
     </div >
