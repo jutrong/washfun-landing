4aa3dca807a5af13fb465dd5d4d5c635bab269b7
[jutrong] <[leek71358@gmail.com]>
2024-11-22 14:39:22 +0900

    Chore: Webview terms-of-service 추가

---
+++ src/app/(webview)/terms-of-service/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+const TermsOfService = () => {
+  return (
+    <div className="flex size-full justify-center">
+      <div className="flex w-[1200px] justify-center">
+        <Image src="/images/png/termsOfService.png" width={1900} height={10000} alt="policy" />
+      </div>
+    </div>
+  )
+}
+
+export default TermsOfService;
